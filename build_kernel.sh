1070f29dfeddc1882ef1d90cc451999275a74b6e
oberdfr <alebennella@gmail.com>
2024-09-29 18:17:30 +0200

    build_kernel: build out

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,5 +5,5 @@ export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
 export SEC_BUILD_CONF_VENDOR_BUILD_OS=13
 
-make ARCH=arm64 exynos2100-p3sxxx_defconfig
-make ARCH=arm64 -j16
+make O=out ARCH=arm64 exynos2100-p3sxxx_defconfig
+make O=out ARCH=arm64 -j16
