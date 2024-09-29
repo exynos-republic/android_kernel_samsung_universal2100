0a60526b3b6979246e0a6e2f1657079919f20f61
oberdfr <alebennella@gmail.com>
2024-09-29 18:18:56 +0200

    build_kernel: add more definitions

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,6 +4,13 @@ export ARCH=arm64
 export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
 export SEC_BUILD_CONF_VENDOR_BUILD_OS=13
+export CC=clang 
+export LD=ld.lld 
+export LLVM=1 
+export LLVM_IAS=1 
+export CLANG_TRIPLE=aarch64-linux-gnu- 
+export CROSS_COMPILE=aarch64-linux-gnu- 
+export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 
 make O=out ARCH=arm64 exynos2100-o1sxxx_defconfig
 make O=out ARCH=arm64 -j16
